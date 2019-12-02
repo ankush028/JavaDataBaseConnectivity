b89bcd3cb711fd4975c6ae193ad406555d022c1f
Ankush Agrawal <akag0284@gmail.com>
2019-12-02 17:04:17 +0530

    Update CallableFetch.java

--- Jdbc/src/com/bridgelabz/jdbc/CallableStatement/CallableFetch.java
+++ Jdbc/src/com/bridgelabz/jdbc/CallableStatement/CallableFetch.java
@@ -9,11 +9,15 @@ public class CallableFetch {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ankush", "root", "password");
 		CallableStatement stmt = con.prepareCall("{CALL `ankush`.`fetch`()}");
-		
+		try{
 		ResultSet rs = stmt.executeQuery();
-		while (rs.next()) {
+			while (rs.next()) {
 			System.out.println(rs.getInt(1) + " : " + rs.getString(2));
 		}
+		}finally{
+			jcon.close();	
+			}
+		
 
 	}
 
